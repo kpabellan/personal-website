22bae71e3bfcd06cf54845dc146f4bd463946801
Kobey Pabellan <pabellank@gmail.com>
2023-01-09 01:15:32 -0800

    Implemented Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import Script from 'next/script'
 import { ThemeProvider } from 'next-themes'
 import { Layout } from '../components/Layout'
 import { Comfortaa } from '@next/font/google'
@@ -11,6 +12,18 @@ const comfortaa = Comfortaa({
 export default function App({ Component, pageProps }) {
   return (
     <main className={`${comfortaa.variable} font-arial`}>
+
+      <Script strategy='afterInteractive' src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_PROPERTY_ID}`} />
+
+      <Script id='google-analytics' strategy='afterInteractive'>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_PROPERTY_ID}');
+        `}
+      </Script>
+
       <ThemeProvider enableSystem={true} attribute='class'>
         <Layout>
           <Component {...pageProps} />
