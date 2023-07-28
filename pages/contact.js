72aa86175261fe0f5beabf0d4ad3bbe274b4469d
Kobey Pabellan <pabellank@gmail.com>
2023-07-28 05:06:17 -0700

    Added animation to contact page

--- pages/contact.js
+++ pages/contact.js
@@ -1,9 +1,16 @@
 import { Contact } from '../components/Contact';
+import { motion } from 'framer-motion';
 
 export default function contact() {
   return (
     <>
-      <Contact />
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+      >
+        <Contact />
+      </motion.div>
     </>
   );
 }
