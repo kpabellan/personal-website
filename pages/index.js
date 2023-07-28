ba03289fc988957459bf7a2d6ea28481b9cff5ab
Kobey Pabellan <pabellank@gmail.com>
2023-07-28 04:53:26 -0700

    Increased duration of animation

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
       <motion.div
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5 }}
+        transition={{ duration: 1 }}
       >
         <Introduction />
         <Projects />
