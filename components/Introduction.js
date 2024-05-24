d1b8051fa32dd42afe4a3ba445a34207ea8f10bc
Kobey Pabellan <pabellank@gmail.com>
2024-05-24 03:51:55 -0700

    Intro and resume update

--- components/Introduction.js
+++ components/Introduction.js
@@ -17,7 +17,7 @@ export const Introduction = () => {
       </h1>
 
       <p className='mx-auto text-left text-xl'>
-      I&apos;m a computer science and engineering student at <a href='https://www.ucmerced.edu/about' target='_blank' rel='noopener noreferrer' className='underline hover:no-underline hover:text-gray-400'> University of California, Merced</a>. From building to breaking, I have a strong passion for both software development and cybersecurity. In my free time, I&apos;m often working on projects or learning new skills and technologies. Outside the digital realm, I enjoy hiking and spending my time outdoors. For any inquiries, please don&apos;t hesitate to reach out to me on my social media profiles.
+      I&apos;m a recent computer science and engineering graduate at <a href='https://www.ucmerced.edu/about' target='_blank' rel='noopener noreferrer' className='underline hover:no-underline hover:text-gray-400'> University of California, Merced</a>. From building to breaking, I have a strong passion for both software development and cybersecurity. In my free time, I&apos;m often working on projects or learning new skills and technologies. Outside the digital realm, I enjoy hiking and spending my time outdoors. For any inquiries, please don&apos;t hesitate to reach out to me on my social media profiles.
       </p>
     </div>
   );
