a953df7fc70bc3464afc011f6ff9dc4962727712
Kobey Pabellan <pabellank@gmail.com>
2025-03-04 02:09:13 -0800

    Added icon

--- pages/_document.js
+++ pages/_document.js
@@ -3,8 +3,14 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className='scrollbar'>
+      <Head>
+        <link rel="icon" href="/kpicon.ico" type="image/x-icon" />
+        <meta property="og:image" content="/kpicon.ico" />
+        <meta property="og:image:type" content="image/x-icon" />
+        <meta property="og:image:width" content="300" />
+        <meta property="og:image:height" content="300" />
+      </Head>
+      <body className="scrollbar">
         <Main />
         <NextScript />
       </body>
